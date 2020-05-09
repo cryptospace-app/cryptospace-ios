9e01f7a36212b981fc75f023747c1ecae8dff6e1
Ivan Grachyov <mail@grachyov.com>
2020-05-09 23:18:20 +0300

    Start with enter key screen

--- cryptospace/SceneDelegate.swift
+++ cryptospace/SceneDelegate.swift
@@ -10,7 +10,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = scene as? UIWindowScene else { return }
         let window = UIWindow(windowScene: scene)
-        window.rootViewController = instantiate(CreateSpaceViewController.self)
+        window.rootViewController = instantiate(EnterKeyViewController.self).inNavigationController
         self.window = window
         window.makeKeyAndVisible()
     }
