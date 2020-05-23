33b1a327ade034bdc09b49635bdca16e0a17f9ae
Vadim Zakharenko <vadim_zakhar@mail.ru>
2020-05-24 02:20:09 +0300

    Validate private key, add ens stub.

--- cryptospace/Screens/EnterKeyViewController.swift
+++ cryptospace/Screens/EnterKeyViewController.swift
@@ -25,12 +25,40 @@ class EnterKeyViewController: UIViewController {
     // TODO: rise button with keyboard - Ivan has code for that
     
     @IBAction func textFieldEditingChanged(_ sender: Any) {
-        
+        if isValidPrivateKey(textField.text ?? "") {
+            enterButton.setTitle("Next", for: .normal)
+        } else {
+            enterButton.setTitle("Paste", for: .normal)
+        }
     }
     
+    private func isValidPrivateKey(_ key: String) -> Bool {
+        if let _ = try? EthPrivateKey(hex: key).address().value() {
+            return true
+        } else {
+            return false
+        }
+    }
     
     @IBAction func textFieldEditingDidEnd(_ sender: Any) {
         
+    }
+    
+    private let ens = EthereumNameService(
+        client: EthereumClient(url:
+            URL(string: "https://ropsten.infura.io/v3/3f99b6096fda424bbb26e17866dcddfc")!
+        ),
+        registryAddress: EthereumAddress("0x112234455c3a32fd11230c42e7bccd4a84e02010")
+    )
+    
+    private func getENSName(address: String, completion: @escaping (String?) -> Void) {
+        ens.resolve(address: EthereumAddress(address)) { error, result  in
+            if error != nil || result == nil {
+                completion(nil)
+            } else {
+                completion(result)
+            }
+        }
     }
     
     @IBAction func enterButtonTapped(_ sender: Any) {
@@ -40,8 +68,13 @@ class EnterKeyViewController: UIViewController {
             return
         }
         
-        if let _ = try? EthPrivateKey(hex: text).address().value() {
+        if let address = try? EthPrivateKey(hex: text).address().value().toHexString() {
             Defaults.privateKey = text
+            
+            
+            getENSName(address:"0x" + address) { result in
+                
+            }
             // TODO: retreive ens name after address is known. if there is no ens name, then push name screen
             if false {
                 let enterKahoot = instantiate(EnterKahootViewController.self)
