d7b78709f8b4248f8bf0735401998f749b587bf3
Ivan Grachev <ivan@ivan.lol>
2020-05-24 18:54:42 +0300

    Add TODO to Space screen

--- cryptospace/Screens/SpaceViewController.swift
+++ cryptospace/Screens/SpaceViewController.swift
@@ -32,9 +32,34 @@ class SpaceViewController: UIViewController {
         }
     }
     
+    // TODO: на viewWillAppear и каждые четыре секунды делать запросы: (не делать запросы, когда чувак ушел на GameViewController)
+    // - isFinished
+    // - players
+    // - winner
+    // - челендж с кахута
+    
+    // записывать все полученные данные в переменные
+    
+    // регулярно обновлять таблицу: показывать текущие результаты с кахута, добавлять к ним игроков с контракта, которые еще не появились в ответе кахута
+    
+    // в зависимости от состояния игры, при каждом обновлении данных, обновлять текст кнопки и действие при нажатии на нее (завести enum)
+    
+    // состояния кнопки:
+    // - play (если еще не сыграл). она в самом начале, вызывает openGame()
+    // - leave (если уже точно проебал) —
+    // - get prize (если уже точно выиграл) — вызывает sendPrize, кнопка переходит в waiting state, пока не отправилась транзакция. если не отправилась, кнопка возвращается в обычное состояние, показывается сообщение об ошибке.
+    // - спрятана (когда уже сыграл, но кто-то другой еще не доиграл)
+    // - finish (если игра оказалась нечестной) — отправляется транзакция sendPrize. если не отправилась, просто вызывается leaveGame
+    
     private func openGame() {
         let game = instantiate(GameViewController.self)
         present(game, animated: true)
+    }
+    
+    private func leaveGame() {
+        guard let enterKahoot = navigationController?.viewControllers.first(where: { $0 is EnterKahootViewController }) else { return }
+        Defaults.kahootId = nil
+        navigationController?.popToViewController(enterKahoot, animated: true)
     }
     
     @IBAction func actionButtonTapped(_ sender: Any) {
