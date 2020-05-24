799afe9acae639ca6e78dc7dbec0f87156d4bdc3
Ivan Grachev <ivan@ivan.lol>
2020-05-24 17:58:59 +0300

    Add GameViewController

---
+++ cryptospace/Screens/GameViewController.swift
@@ -0,0 +1,12 @@
+// Copyright © 2020 cryptospace. All rights reserved.
+
+import UIKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Play"
+    }
+    
+}
