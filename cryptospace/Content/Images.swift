a72fab72220eb5412f4c4b558955b4ee3073bc04
Ivan Grachev <ivan@ivan.lol>
2020-05-25 03:23:55 +0300

    Add showErrorMessage

--- cryptospace/Content/Images.swift
+++ cryptospace/Content/Images.swift
@@ -5,6 +5,7 @@ import UIKit
 struct Images {
     
     static var okIndicator: UIImage { systemName("checkmark.circle") }
+    static var errorIndicator: UIImage { systemName("exclamationmark.triangle") }
     
     private static func systemName(_ systemName: String) -> UIImage {
         return UIImage(systemName: systemName)!
